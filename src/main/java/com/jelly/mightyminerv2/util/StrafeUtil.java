7696ab7acdb2e5061a9a93976dcf8fd03b67b910
Gaurav Chauhan <109675321+GauravChauhan4795@users.noreply.github.com>
2025-12-28 18:22:31 +0530

    Add strength variable to StrafeUtil

--- src/main/java/com/jelly/mightyminerv2/util/StrafeUtil.java
+++ src/main/java/com/jelly/mightyminerv2/util/StrafeUtil.java
@@ -5,6 +5,7 @@ public final class StrafeUtil {
     public static volatile boolean enabled = false;
     public static volatile boolean forceStop = false;
     public static volatile float yaw = 0.0f;
+    public static float strength = 1.0f;
 
     public static boolean shouldEnable() {
         return !forceStop && enabled;
