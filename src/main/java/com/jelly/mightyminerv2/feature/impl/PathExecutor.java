c618a858b055258206b12e89b4e17af26da68df0
Gaurav Chauhan <109675321+GauravChauhan4795@users.noreply.github.com>
2025-12-28 18:32:18 +0530

    fix variables

--- src/main/java/com/jelly/mightyminerv2/feature/impl/PathExecutor.java
+++ src/main/java/com/jelly/mightyminerv2/feature/impl/PathExecutor.java
@@ -28,8 +28,8 @@ public class PathExecutor {
     private final Map<Long, List<Long>> map = new HashMap<>();
     private final List<BlockPos> blockPath = new ArrayList<>();
     private final Clock stuckTimer = new Clock();
-    private final DelayTimer nodeSwitchDelay = new DelayTimer();
-    private final DelayTimer jumpDelay = new DelayTimer();
+    private final Clock nodeSwitchDelay = new Clock();
+    private final Clock jumpDelay = new Clock();
     @Getter
     private boolean enabled = false;
     private Path prev;
